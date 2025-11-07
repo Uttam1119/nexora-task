b1c50a9bfbbb718fa6280133fa72d9919b4bb6a3
Uttam <kumaruttam519@gmail.com>
2025-11-08 03:19:53 +0530

    update: added database file and different routes file

---
+++ backend/src/routes/cartRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getCart,
+  addToCart,
+  updateCartItem,
+  deleteCartItem,
+} = require("../controllers/cartController");
+
+router.get("/", getCart);
+router.post("/", addToCart);
+router.put("/:id", updateCartItem);
+router.delete("/:id", deleteCartItem);
+
+module.exports = router;
